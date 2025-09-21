182649efc5f553a399d81b8786e2a030e86e6515
Giuliano <giulianodeamicis2009@gmail.com>
2025-09-21 18:16:12 +0200

    feat: completed the first section of the steam engine + updated the sidebar

--- src/scripts/sidebar/init.ts
+++ src/scripts/sidebar/init.ts
@@ -1,13 +1,15 @@
 import { toggleNode } from "./Toggle-Node";
 
-// seleziona tutti i wrapper della sidebar
 document.querySelectorAll<HTMLElement>('.sidebar-wrapper').forEach(wrapper => {
   const id = wrapper.dataset.id;
   if (!id) return;
 
-  // evita di aggiungere più volte lo stesso listener
   if ((wrapper as any).__listenerAttached) return;
 
-  wrapper.addEventListener('click', () => toggleNode(id));
+  wrapper.addEventListener('click', (event) => {
+    event.stopPropagation();
+    toggleNode(id);
+  });
+
   (wrapper as any).__listenerAttached = true;
 });
