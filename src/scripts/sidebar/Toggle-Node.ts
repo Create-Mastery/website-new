afb95541243a76d40518f1f98fc95f81482be561
Giuliano <giulianodeamicis2009@gmail.com>
2025-09-21 17:05:43 +0200

    feat: now the node for nodes have a corret logic for opening them, and the kinetic energy node will be open on the kinetic energy page

---
+++ src/scripts/sidebar/Toggle-Node.ts
@@ -0,0 +1,11 @@
+export function toggleNode(id: string) {
+  const chevronIcon = document.getElementById("chevronIcon-" + id);
+  const entryContent = document.getElementById("id-node-" + id);
+
+  if (!chevronIcon || !entryContent) return;
+
+  const isOpen = entryContent.style.display === "block";
+
+  entryContent.style.display = isOpen ? "none" : "block";
+  chevronIcon.style.setProperty("--fa-rotate-angle", isOpen ? "-90deg" : "0deg");
+}
