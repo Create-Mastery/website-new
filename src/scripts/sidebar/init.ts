21d7f12c19e3d3c374a50ff5a73bcbbf0bc7ea4b
Sckab <giulianodeamicis2009@gmail.com>
2025-09-25 21:14:16 +0200

    feat: formatted all the site and added support for prettier

--- src/scripts/sidebar/init.ts
+++ src/scripts/sidebar/init.ts
@@ -1,15 +1,17 @@
 import { toggleNode } from "./Toggle-Node";
 
-document.querySelectorAll<HTMLElement>('.sidebar-wrapper').forEach(wrapper => {
-  const id = wrapper.dataset.id;
-  if (!id) return;
+document
+  .querySelectorAll<HTMLElement>(".sidebar-wrapper")
+  .forEach((wrapper) => {
+    const id = wrapper.dataset.id;
+    if (!id) return;
 
-  if ((wrapper as any).__listenerAttached) return;
+    if ((wrapper as any).__listenerAttached) return;
 
-  wrapper.addEventListener('click', (event) => {
-    event.stopPropagation();
-    toggleNode(id);
+    wrapper.addEventListener("click", (event) => {
+      event.stopPropagation();
+      toggleNode(id);
+    });
+
+    (wrapper as any).__listenerAttached = true;
   });
-
-  (wrapper as any).__listenerAttached = true;
-});
